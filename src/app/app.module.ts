5ec535b1ca70f8bf1368318ba543578e9c64a5b2
Yegor Dargin <darginyegor@gmail.com>
2020-12-15 21:28:07 +0300

    generate basic project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { WeatherListComponent } from './forecast/weather-list/weather-list.component';
+import { WeatherItemComponent } from './forecast/weather-list/weather-item/weather-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForecastComponent,
+    WeatherListComponent,
+    WeatherItemComponent
   ],
   imports: [
     BrowserModule
